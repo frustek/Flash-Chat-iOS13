ef5b8c25ab62ff66283be649021150737654e303
Adam Mroz <MrozAdam1992@gmail.com>
2022-02-11 11:57:43 +0100

    Initial screen animation

--- Flash Chat iOS13/Controllers/WelcomeViewController.swift
+++ Flash Chat iOS13/Controllers/WelcomeViewController.swift
@@ -15,6 +15,16 @@ class WelcomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let titleText = "⚡️FlashChat"
+        titleLabel.text = ""
+        var charIndex = 0 as Double
+        for letter in titleText {
+            
+            Timer.scheduledTimer(withTimeInterval: 0.1 * charIndex, repeats: false) { Timer in
+                self.titleLabel.text?.append(letter)
+            }
+            charIndex += 1
+        }
        
     }
     
