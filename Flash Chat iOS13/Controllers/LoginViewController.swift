39aac11d29e16b610b0b637efec69bf654972d32
Adam Mroz <MrozAdam1992@gmail.com>
2022-04-25 21:01:17 +0200

    Login logic

--- Flash Chat iOS13/Controllers/LoginViewController.swift
+++ Flash Chat iOS13/Controllers/LoginViewController.swift
@@ -17,6 +17,16 @@ class LoginViewController: UIViewController {
 
     @IBAction func loginPressed(_ sender: UIButton) {
         
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().signIn(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    print(e)
+                } else{
+                    self.performSegue(withIdentifier: "LoginToChat", sender: self)
+                }
+                
+            }
+        }
     }
     
 }
