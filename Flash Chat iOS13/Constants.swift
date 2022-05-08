87f686734ee9d8e3cb2cb3d04bd99a4af0426415
Adam Mroz <MrozAdam1992@gmail.com>
2022-05-08 13:22:46 +0200

    Constants

---
+++ Flash Chat iOS13/Constants.swift
@@ -0,0 +1,30 @@
+//
+//  Constants.swift
+//  Flash Chat iOS13
+//
+//  Created by Adam Mroz on 08/05/2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+struct K {
+    
+    static let appname = "⚡️FlashChat"
+    static let cellIdentifier = "ReusableCell"
+    static let cellNibName = "MessageCell"
+    static let registerSegue = "RegisterToChat"
+    static let loginSegue = "LoginToChat"
+    
+    struct BrandColors {
+        static let purple = "BrandPurple"
+        static let lightPurple = "BrandLightPurple"
+        static let blue = "BrandBlue"
+        static let lighBlue = "BrandLightBlue"
+    }
+    
+    struct FStore {
+        static let collectionName = "messages"
+        static let senderField = "sender"
+        static let bodyField = "body"
+        static let dateField = "date"
+    }
+}
