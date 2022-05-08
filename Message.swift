92ba4593aa12fb564a42576e5c8ed8506cd90a66
Adam Mroz <MrozAdam1992@gmail.com>
2022-05-08 14:19:51 +0200

    TableView implementation

---
+++ Message.swift
@@ -0,0 +1,15 @@
+//
+//  Message.swift
+//  Flash Chat iOS13
+//
+//  Created by Adam Mroz on 08/05/2022.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    
+    let sender: String
+    let body: String
+}
