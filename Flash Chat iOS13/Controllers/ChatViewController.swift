96e3bf7aa41ec0870d6f772b6db8838480f3a221
Adam Mroz <MrozAdam1992@gmail.com>
2022-07-27 21:31:59 +0200

    Message view changes

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -26,7 +26,7 @@ class ChatViewController: UIViewController {
         tableView.dataSource = self
         title = K.appname
         navigationItem.hidesBackButton = true
-
+        tableView.register(UINib(nibName: K.cellNibName, bundle: nil), forCellReuseIdentifier: K.cellIdentifier)
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
@@ -52,8 +52,8 @@ extension ChatViewController: UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath)
-        cell.textLabel?.text = messages[indexPath.row].body
+        let cell = tableView.dequeueReusableCell(withIdentifier: K.cellIdentifier, for: indexPath) as! MessageCell
+        cell.lebel.text = messages[indexPath.row].body
         return cell
     }
     
