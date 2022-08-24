a89321905f3eb918cf57584d52ac3ee112008d1d
Adam Mroz <MrozAdam1992@gmail.com>
2022-08-24 22:17:30 +0200

    IQKeyboardManager package added.

--- Flash Chat iOS13/AppDelegate.swift
+++ Flash Chat iOS13/AppDelegate.swift
@@ -8,18 +8,21 @@
 
 import UIKit
 //import Firebase
-
+import IQKeyboardManagerSwift
 import FirebaseCore
 import FirebaseFirestore
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.enableAutoToolbar = false
+        IQKeyboardManager.shared.shouldResignOnTouchOutside = true
         FirebaseApp.configure()
         let db = Firestore.firestore()
         
